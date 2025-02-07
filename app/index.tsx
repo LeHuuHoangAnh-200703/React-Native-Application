62b79c451ef52f0644bd2f67d4e363bedb69d6f0
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-02-07 09:18:34 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+const index = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text className='text-red-600 font-bold text-2xl'>Hello world</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default index;
